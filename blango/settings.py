5680c877e1f3a898bb261072d601a0b568a79978
RODRIGO BARATTA <rodrigo.baratta@bcb.gov.br>
2023-01-04 16:53:11 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -28,7 +28,13 @@ class Dev(Configuration):
   SESSION_COOKIE_SECURE = True
   CSRF_COOKIE_SAMESITE = 'None'
   SESSION_COOKIE_SAMESITE = 'None'
-  
+
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
+
   PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
@@ -83,12 +89,17 @@ class Dev(Configuration):
       "django.contrib.contenttypes",
       "django.contrib.sessions",
       "django.contrib.messages",
+      "django.contrib.sites",
       "django.contrib.staticfiles",
       "blango_auth",
       "blog",
       "crispy_forms",
       "crispy_bootstrap5",
       "debug_toolbar",
+      "allauth",
+      "allauth.account",
+      "allauth.socialaccount",
+      "allauth.socialaccount.providers.google"
   ]
 
   MIDDLEWARE = [
